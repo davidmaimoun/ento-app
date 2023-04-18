b570ce51fc5e7a194d7ebef99c11f7b8015c3069
user1 <davidmaimoun@hotmail.com>
2023-04-18 11:26:12 +0300

    commit

--- app.py
+++ app.py
@@ -51,77 +51,77 @@ import streamlit as st
 st.write("HERE")
 
 # if files is not None:
-    txt_files = []
-    excel_files = []
-    txt_names = []
-    df_combined = pd.DataFrame()
-    df_txt = pd.DataFrame()
-    df_excel = pd.DataFrame()
-    is_empty = True
+#     txt_files = []
+#     excel_files = []
+#     txt_names = []
+#     df_combined = pd.DataFrame()
+#     df_txt = pd.DataFrame()
+#     df_excel = pd.DataFrame()
+#     is_empty = True
 
-    for file in files:
-        if '.txt' in file.name: 
-            txt_files.append(file)
-            txt_names.append(file.name)
-        if ('.xls' in file.name) or ('.xlsx' in file.name):
-            excel_files.append(file)
+#     for file in files:
+#         if '.txt' in file.name: 
+#             txt_files.append(file)
+#             txt_names.append(file.name)
+#         if ('.xls' in file.name) or ('.xlsx' in file.name):
+#             excel_files.append(file)
 
-    unique_txt = set(txt_names)
-    count_unique_txt = len(unique_txt)
-    df = pd.DataFrame()
+#     unique_txt = set(txt_names)
+#     count_unique_txt = len(unique_txt)
+#     df = pd.DataFrame()
 
-    if (len(txt_files) == 3) & (len(txt_files) == count_unique_txt):
-        df1 = pd.read_csv(txt_files[0], delimiter = '\t')
-        df2 = pd.read_csv(txt_files[1], delimiter = '\t')
-        df3 = pd.read_csv(txt_files[2], delimiter = '\t')
+#     if (len(txt_files) == 3) & (len(txt_files) == count_unique_txt):
+#         df1 = pd.read_csv(txt_files[0], delimiter = '\t')
+#         df2 = pd.read_csv(txt_files[1], delimiter = '\t')
+#         df3 = pd.read_csv(txt_files[2], delimiter = '\t')
        
         
-        df = df1.merge((df2.merge(df3, on = POSITION)), on = POSITION)
+#         df = df1.merge((df2.merge(df3, on = POSITION)), on = POSITION)
   
-        mask = [SAMPLE_NAME, POSITION, GENE_NAME, GROUP, CQ, TM]
+#         mask = [SAMPLE_NAME, POSITION, GENE_NAME, GROUP, CQ, TM]
 
-        if len(excel_files) > 0:
-            for excel_file in excel_files:
-                df = pd.read_excel(excel_file)
-                df.rename(columns={
-                    "SampleTubeID": SAMPLE_NAME,
-                    "Test Name": GENE_NAME,
-                    "SampleTubePositionID": POSITION}, inplace=True)
-                df_excel= pd.concat([df_excel, df])
+#         if len(excel_files) > 0:
+#             for excel_file in excel_files:
+#                 df = pd.read_excel(excel_file)
+#                 df.rename(columns={
+#                     "SampleTubeID": SAMPLE_NAME,
+#                     "Test Name": GENE_NAME,
+#                     "SampleTubePositionID": POSITION}, inplace=True)
+#                 df_excel= pd.concat([df_excel, df])
 
-            mask = [SAMPLE_NAME, POSITION, GENE_NAME, GROUP, CQ, TM, COMMENTS]
+#             mask = [SAMPLE_NAME, POSITION, GENE_NAME, GROUP, CQ, TM, COMMENTS]
 
-        is_empty = False
-    else:
-        st.info(f"""
-            Hi, dear user !\n
-            You need to upload these 3 files in 'txt' format:
-              - High Resolution Melting.txt
-              - Quality_detection.txt
-              - TM_calling.txt
-            And all the excels ('xls', 'xlsx') you want.
-        """, icon='🤖')
+#         is_empty = False
+#     else:
+#         st.info(f"""
+#             Hi, dear user !\n
+#             You need to upload these 3 files in 'txt' format:
+#               - High Resolution Melting.txt
+#               - Quality_detection.txt
+#               - TM_calling.txt
+#             And all the excels ('xls', 'xlsx') you want.
+#         """, icon='🤖')
 
-        st.markdown(f"""
-            <p><i>Valid upload ({count_unique_txt}/3)<br> 
-                {"" if count_unique_txt == 0 else unique_txt}</i></p>
-            """, unsafe_allow_html=True)
+#         st.markdown(f"""
+#             <p><i>Valid upload ({count_unique_txt}/3)<br> 
+#                 {"" if count_unique_txt == 0 else unique_txt}</i></p>
+#             """, unsafe_allow_html=True)
 
  
 
-    if is_empty is False:
-        df_combined = pd.concat([df, df_excel])
-        df_combined = df_combined[mask].fillna('')
-        df_final = sortDataFrameBy(df_combined, POSITION)
+#     if is_empty is False:
+#         df_combined = pd.concat([df, df_excel])
+#         df_combined = df_combined[mask].fillna('')
+#         df_final = sortDataFrameBy(df_combined, POSITION)
 
-        st.markdown("<h5>&#128196; Data</h5>", unsafe_allow_html=True)
-        st.dataframe(df_final.reset_index(drop=True))
+#         st.markdown("<h5>&#128196; Data</h5>", unsafe_allow_html=True)
+#         st.dataframe(df_final.reset_index(drop=True))
 
-        csv = convert_df(df_final)
+#         csv = convert_df(df_final)
 
-        st.download_button(
-            label="📥 Download Table",
-            data=csv,
-            file_name='data_combined.csv',
-            mime='text/csv',
-)
+#         st.download_button(
+#             label="📥 Download Table",
+#             data=csv,
+#             file_name='data_combined.csv',
+#             mime='text/csv',
+# )
